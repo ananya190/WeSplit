cfe7b547a1d65dd01c6978cd8bdf9dca6d342b91
Ananya George <ananyageorge019@gmail.com>
2021-07-29 16:25:48 +0530

    created basic WeSplit application

---
+++ WeSplit/WeSplit/ContentView.swift
@@ -0,0 +1,86 @@
+//
+//  ContentView.swift
+//  WeSplit
+//
+//  Created by Ananya George on 7/29/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @State private var checkAmt = ""
+    @State private var numberOfPeople = ""
+    @State private var tipPercentage = 2
+    
+    let tipPercentages = [10, 15, 20, 25, 0]
+    
+    var grandTotal: Double {
+        
+        let tipSelection = Double(tipPercentages[tipPercentage])
+        let orderAmount = Double(checkAmt) ?? 0
+        let tipValue = orderAmount / 100 * tipSelection
+        let grandTotal = orderAmount + tipValue
+        
+        return grandTotal
+        
+    }
+    
+    
+    var totalPerPerson: Double {
+        
+        let peopleCount = Double(numberOfPeople) ?? 0 + 2
+        let amountPerPerson = grandTotal / peopleCount
+        
+        return amountPerPerson
+        
+    }
+    
+    var body: some View {
+        
+        NavigationView {
+            
+            Form {
+                
+                Section {
+                    
+                    TextField("Amount", text: $checkAmt)
+                        .keyboardType(.decimalPad)
+                    
+                    
+                    
+                    TextField("Number of People", text: $numberOfPeople)
+                        .keyboardType(.decimalPad)
+                }
+                
+                Section(header: Text("How much tip do you want to leave?")) {
+                    Picker("Tip Percentage", selection: $tipPercentage) {
+                        
+                        ForEach(0 ..< tipPercentages.count) {
+                            Text("\(self.tipPercentages[$0])%")
+                        }
+                    }
+                    .pickerStyle(SegmentedPickerStyle())
+                    
+                }
+                
+                Section(header: Text("Grand Total")) {
+                    Text("$\(grandTotal, specifier: "%.2f")")
+                }
+                
+                Section(header: Text("Total Per Person")) {
+                    Text("$\(totalPerPerson, specifier: "%.2f")")
+                }
+            }
+            .navigationTitle("WeSplit")
+        }
+        
+        
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WeSplit/WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Ananya George on 7/29/21.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
